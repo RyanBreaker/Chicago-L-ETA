7978675f2d6fe951796809debcfa79c9a9f57583
Ryan Breaker <ryan@breaker.rocks>
2020-01-13 09:22:18 -0600

    Fix frontend spacing issue

--- client/src/components/EtaDisplay.js
+++ client/src/components/EtaDisplay.js
@@ -3,7 +3,7 @@ import { GiRss as LiveIcon } from 'react-icons/gi';
 import { FaRegClock as ClockIcon } from 'react-icons/fa';
 
 const EtaDisplay = props => (
-  <div>
+  <div className="eta-display">
     {/* If the train is flagged as Due, insert "Due" instead of the difference in time. */}
     {props.due ? (
       <span className="font-weight-bold">Due</span>
@@ -12,7 +12,7 @@ const EtaDisplay = props => (
         {/* Difference in time in minutes. */}
         {Math.round(
           (new Date(props.eta) - new Date(props.generatedAt)) / 1000 / 60
-        )}{' '}
+        )}
         <span className="font-weight-normal">min</span>
       </span>
     )}
