1f7dacde36b6a3507f6d9fb393589e3d46ef967f
Ryan Breaker <ryan@breaker.rocks>
2020-01-15 10:52:22 -0600

    React refactor

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ import ScrollUpButton from 'react-scroll-up-button';
 function App() {
   return (
     <div>
-      <Container fluid={true}>
+      <Container fluid>
         <Header />
         <StationList />
       </Container>
