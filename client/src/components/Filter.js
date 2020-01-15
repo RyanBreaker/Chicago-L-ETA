fa4c4d77ac8b384ad735f90a0408e0738b774cab
Ryan Breaker <ryan@breaker.rocks>
2020-01-14 23:51:47 -0600

    Adjust filter input styling

--- client/src/components/Filter.js
+++ client/src/components/Filter.js
@@ -4,14 +4,16 @@ import { Col, Form, Row } from 'react-bootstrap';
 const SearchBar = props => {
   // Current value of the text input.
   const [text, setText] = useState('');
-  // Timeout for auto-filtering the page on input.
+  // Timeout for auto-filtering the page on input change.
   const [timeout, setTimeout] = useState(null);
 
   return (
-    <Form inline={true} onSubmit={e => e.preventDefault()}>
+    <Form onSubmit={e => e.preventDefault()}>
       <Form.Group>
         <Form.Control
+          autoFocus
           type="text"
+          className="form-control-lg"
           placeholder="Station Name"
           onChange={e => setText(e.target.value)}
           onKeyUp={() => {
